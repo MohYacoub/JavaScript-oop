c5635790c2f08d32c96f265d8d698588b8021cfe
Mohyacoub <mohammad.j.yacoub@gmail.com>
2020-10-19 13:19:35 +0300

    jstask

---
+++ Student.js
@@ -0,0 +1,9 @@
+    class Student {
+            constructor(id, name, Email, mobile_number) {
+                this.id = id;
+                this.name = name;
+                this.Email = Email;
+                this.mobile_number = Number(mobile_number);
+            }
+
+        }
---
+++ Teacher.js
@@ -0,0 +1,8 @@
+
+ class Teacher extends Student {
+            constructor(id, name, Email, mobile_number, Salary, Subjects) {
+                super(id, name, Email, mobile_number);
+                this.Salary = Number(Salary);
+                this.Subjects = Subjects.split(',') ;
+                }
+                          }
