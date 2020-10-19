0dc0c22dcc3aa93d39b58b7c12fbd59e203f9fbc
Mohyacoub <mohammad.j.yacoub@gmail.com>
2020-10-19 14:42:06 +0300

    jstask

---
+++ anothersolution/Student.js
@@ -0,0 +1,26 @@
+import 'Teacher' from './Teacher.js';
+
+class Student {
+    constructor(id, name, email, mobile_number) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.mobile_number = mobile_number;
+    }
+    Name() {
+      return this.name;
+    }
+    Mobile() {
+      return Number(this.mobile_number);
+    }
+  }
+  
+  let student1 = new Student(
+    "123443",
+    "Sara",
+    "email:sara@orange.com",
+    "0777777777"
+  );
+  
+  console.log(student1.Name());
+  console.log(student1.Mobile());
---
+++ anothersolution/Teacher.js
@@ -0,0 +1,27 @@
+export default 'Teacher' ;
+
+class Teacher extends Student {
+    constructor(salary, subjects, id, name, email, mobile_number) {
+      super(id, name, email, mobile_number);
+      this.salary = salary;
+      this.subjects = subjects;
+    }
+    Salary() {
+      return Number(this.salary);
+    }
+    Subjects() {
+      return Array.from(this.subjects.split(","));
+    }
+  }
+
+  let teacher1 = new Teacher(
+    "800",
+    "[English, Arabic, Math, science]",
+    "96432",
+    "Sadi",
+    "Sadi@orange.com",
+    "0777788888"
+  );
+  console.log(teacher1.Salary());
+  console.log(teacher1.Subjects());
+
